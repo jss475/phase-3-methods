2c6c6cad921454a354679ac367d517c7de07829a
JosephShin <joespam9999@gmail.com>
2022-05-15 22:52:37 -0400

    commit tree ccffefb32389058bb2b961805dab9d48082fdb6c

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+
+#greet-programmer
+def greet_programmer
+    puts "Hello, programmer!"
+    # "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+end
+
+def add num1, num2
+    num1 + num2
+end
+
+def halve num
+    if num.class != Integer
+        return nil
+    end
+
+    num/2
+end
+
